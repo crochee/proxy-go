63f0caa799c8638b487984e460c1687eb9a15eff
l30002214 <licongfu@huawei.com>
2021-02-09 11:10:45 +0800

    add shell

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ export log_path=./log/proxy.log
 export log_level=DEBUG
 export GIN_MODE=release
 export config=./conf/config.yml
-#./proxy >/dev/null &
-./proxy >>./log/console.txt 2>&1 &
+#./proxy p >/dev/null &
+./proxy p >>./log/console.txt 2>&1 &
