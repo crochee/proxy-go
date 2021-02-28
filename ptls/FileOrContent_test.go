dbf8023eb6267cd2d8eb1889e8df6922b8792915
OnlyOneFace <13522570308@139.com>
2021-02-28 13:12:52 +0800

    add test

---
+++ ptls/FileOrContent_test.go
@@ -0,0 +1,36 @@
+// Copyright 2021, The Go Authors. All rights reserved.
+// Author: crochee
+// Date: 2021/2/28
+
+package ptls
+
+import "testing"
+
+func TestFileOrContent_Read(t *testing.T) {
+	list := []struct {
+		f    FileOrContent
+		want string
+	}{
+		{
+			f:    "/obs/file/suv/test.txt",
+			want: "11",
+		},
+		{
+			f:    "/obs/file/suv/test1.txt",
+			want: "11",
+		},
+		{
+			f:    "/obs/file/suvsdfgha.23",
+			want: "/obs/file/suvsdfgha.23",
+		},
+	}
+	for _, tt := range list {
+		if got, err := tt.f.Read(); err != nil {
+			t.Error(err)
+		} else {
+			if string(got) != tt.want {
+				t.Fatalf("got %s want %s", got, tt.want)
+			}
+		}
+	}
+}
